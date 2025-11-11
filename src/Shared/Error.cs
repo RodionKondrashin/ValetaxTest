b178d7f6773aa1a8f556d9fd247770d53c12097e
Rodion <rodion.kondrashin@bk.ru>
2025-11-11 21:03:52 +0700

    feat: add shared project and error class

---
+++ src/Shared/Error.cs
@@ -0,0 +1,34 @@
+﻿namespace Shared;
+
+public record Error
+{
+    public string Code { get; }
+    public string Message { get; }
+    public ErrorType Type { get; }
+    public string? InvalidField { get; }
+
+    public Error(string code, string message, ErrorType type, string? invalidField = null)
+    {
+        Code = code;
+        Message = message;
+        Type = type;
+        InvalidField = invalidField;
+    }
+
+    public static Error NotFound(string message, Guid? id) => new("value.not.found", message, ErrorType.NOT_FOUND);
+    
+    public static Error Conflict(string? code, string message) => new(code ?? "value.is.conflict", message, ErrorType.CONFLICT);
+
+    public static Error Validation(string? code, string message) => new (code ?? "value.is.invalid", message, ErrorType.VALIDATION);
+}
+
+public enum ErrorType
+{
+    VALIDATION,
+    
+    NOT_FOUND,
+    
+    FAILURE,
+    
+    CONFLICT,
+}
