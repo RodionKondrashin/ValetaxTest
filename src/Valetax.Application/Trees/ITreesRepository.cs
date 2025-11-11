7ae6a393b9fd44c1485b4d51cf51e9c393470635
Rodion <rodion.kondrashin@bk.ru>
2025-11-11 21:16:04 +0700

    feat: add trees repository interface

---
+++ src/Valetax.Application/Trees/ITreesRepository.cs
@@ -0,0 +1,16 @@
+﻿using CSharpFunctionalExtensions;
+using Shared;
+using ValetaxTest.Domain.Trees;
+
+namespace Valetax.Application.Trees;
+
+public interface ITreesRepository
+{
+    Task<Result<Tree, Error>> GetTreeAsync(string treeName);
+    
+    Task<Result<Node, Error>> AddNodeAsync(string treeName, long? parentNodeId, string nodeName);
+    
+    Task<Result<Node, Error>> RenameNodeAsync(long nodeId, string newNodeName);
+    
+    Task<UnitResult<Error>> DeleteNodeAsync(long nodeId);
+}
