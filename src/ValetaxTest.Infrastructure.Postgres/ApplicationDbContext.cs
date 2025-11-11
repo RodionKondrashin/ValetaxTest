bb128c6a2314b9829ddb5b4898068db86411ad54
Rodion <rodion.kondrashin@bk.ru>
2025-11-11 16:26:20 +0700

    feat: add dbContext

---
+++ src/ValetaxTest.Infrastructure.Postgres/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValetaxTest.Domain.Trees;
+
+namespace ValetaxTest.Infrastructure.Postgres;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public ApplicationDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+    
+    public DbSet<Tree> Trees => Set<Tree>();
+    public DbSet<Node> Nodes => Set<Node>();
+}
