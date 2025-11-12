91c2db0b2a58ab68c0b8358e2a7430a13431c54d
Rodion <rodion.kondrashin@bk.ru>
2025-11-12 12:07:54 +0700

    feat: add exception journal migration

--- src/ValetaxTest.Infrastructure.Postgres/Migrations/20251112050643_AddExceptionJournal.Designer.cs
+++ src/ValetaxTest.Infrastructure.Postgres/Migrations/20251112050643_AddExceptionJournal.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ValetaxTest.Infrastructure.Postgres;
@@ -11,9 +12,11 @@ using ValetaxTest.Infrastructure.Postgres;
 namespace ValetaxTest.Infrastructure.Postgres.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251112050643_AddExceptionJournal")]
+    partial class AddExceptionJournal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
