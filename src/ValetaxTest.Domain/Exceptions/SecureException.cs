6200fe1b1877053f495901ef664654d79b015e35
Rodion <rodion.kondrashin@bk.ru>
2025-11-12 11:32:12 +0700

    feat: add secure exception

---
+++ src/ValetaxTest.Domain/Exceptions/SecureException.cs
@@ -0,0 +1,14 @@
+﻿namespace ValetaxTest.Domain.Exceptions;
+
+public class SecureException : Exception
+{
+    public string Type { get; }
+    
+    public long? Id { get; }
+
+    public SecureException(string type, long? id, string message) : base(message)
+    {
+        Type = type;
+        Id = id;
+    }
+}
