0a815c743d6ce744fc11628c8017ddb2b3006594
Rodion <rodion.kondrashin@bk.ru>
2025-11-12 15:05:20 +0700

    feat: add rangeDto

---
+++ src/ValetaxTest.Domain/ExceptionJournals/Dtos/RangeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ValetaxTest.Domain.ExceptionJournals.Dtos;
+
+public class RangeDto<T>
+{
+    public int SKip { get; set; }
+    
+    public int Count { get; set; }
+
+    public List<T> Items { get; set; } = [];
+}
